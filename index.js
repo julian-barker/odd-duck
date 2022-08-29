4a314570ccdb3fbc023c765d7be84bec6ab7575e
Julian Barker <julian.c.barker@gmail.com>
2022-08-29 12:19:55 -0700

    add assets and scaffolding

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// declare shortcut function
+function $(selector) { return document.querySelector(selector) }
+function $$(selector) { return document.querySelectorAll(selector) }
+function byId(id) { return document.getElementById(id) }
+function _(tag) { return document.createElement(tag) }
+
